3ead360104b2fb9a1c37d38dae0b2c430e5ac5e2
Caleb Serafin <31820984+CalebSerafin@users.noreply.github.com>
2020-03-21 14:46:53 +0200

    Moved String Graphics into shared project.

---
+++ CSS - StringGraphics/StringGraphics.cs
@@ -0,0 +1,61 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRage;
+using VRageMath;
+
+namespace IngameScript {
+	public class StringGraphics {
+		public class ArrowSelector {
+			/// <summary> Default "-->" </summary>
+			public string ArrowText;    
+			public string[] Options;
+			/// <summary> Zero-Based Index </summary>
+			public UInt16 Selection;
+			/// <summary> False = Moving List </summary>
+			public Boolean MovingArrow;
+				
+			public ArrowSelector(string ArrowTextX = "--> ", string[] OptionsX = null, UInt16 SelectionX = 0, Boolean MovingArrowX = true) {
+				ArrowText = ArrowTextX;
+				Options = OptionsX ?? Array.Empty<string>();
+				Selection = SelectionX;
+				MovingArrow = MovingArrowX;
+			}
+
+			public string Render() {
+				string RenderOut = "";
+				string Padding = new String(' ', ArrowText.Length);
+				UInt16 OptionsCount = (UInt16)Options.Count();
+				Selection = (UInt16) (Selection % OptionsCount);
+
+				if (MovingArrow) {
+					for (int i = 0; i < OptionsCount; i++) {
+						RenderOut += ((Selection == i) ? ArrowText : Padding) + Options[i] + '\n';
+					}
+				} else {
+					RenderOut += ArrowText + Options[Selection] + '\n';
+					for (int i = Selection + 1; i < OptionsCount; i++) {
+						RenderOut += Padding + Options[i] + '\n';
+					}
+					for (int i = 0; i < Selection; i++) {
+						RenderOut += Padding + Options[i] + '\n';
+					}
+				}
+				return RenderOut;
+			}
+		}
+	}
+}
