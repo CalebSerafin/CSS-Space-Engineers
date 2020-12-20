0183221f39bda82f538e72768e6bc23612e916ca
Caleb Serafin <calebserafin@outlook.com>
2020-12-20 14:59:48 +0200

    Worked on projectile motion, still no luck.

---
+++ CSS - SBS-S2M4/Diagnostics.cs
@@ -0,0 +1,65 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRage;
+using VRageMath;
+using CSS_Common;
+
+namespace IngameScript {
+	public static class Diagnostics {
+		public static void FormatDumpData(ref StringBuilder DataDump, string Name, string Data) {
+			DataDump.Append(string.Format("---BEGIN {1} DUMP BLOCK---{0}", Environment.NewLine, Name));
+			DataDump.Append(Data);
+			DataDump.Append(string.Format("---END {1} DUMP BLOCK---{0}", Environment.NewLine, Name));
+		}
+		public static void FormatDumpData<T>(ref StringBuilder DataDump, string Name, IEnumerable<T> Data) where T : class {
+			FormatDumpData(ref DataDump, Name, Data?.Aggregate("", (TextBuffer, Item) => TextBuffer + Item?.ToString() + Environment.NewLine));
+		}
+		public static void FormatDumpData<T, Y>(ref StringBuilder DataDump, string Name, IEnumerable<KeyValuePair<T, Y>> Data) {
+			FormatDumpData(ref DataDump, Name, Data?.Aggregate("", (TextBuffer, Pair) => TextBuffer + Pair.ToString() + Environment.NewLine));
+		}
+		public static void FormatDumpData(ref StringBuilder DataDump, string Name, UpdateEventHandlers Data) {
+			FormatDumpData(ref DataDump, $"{Name}.Subscribers", Data?.Subscribers); // Default Event Subscribers are not shown.
+		}
+		public static void FormatDumpData(ref StringBuilder DataDump, string Name, UpdateFrequency Data) {
+			FormatDumpData(ref DataDump, Name, Data.ToString() + Environment.NewLine);
+		}
+		public static void FormatDumpData(ref StringBuilder DataDump, string Name, UpdateType Data) {
+			FormatDumpData(ref DataDump, Name, Data.ToString() + Environment.NewLine);
+		}
+	}
+	partial class Program : MyGridProgram {
+		void DumpData(object Sender, UpdateEventArgs e) {
+			if (!e.argument.Equals("-DumpData", StringComparison.OrdinalIgnoreCase)) return;
+			// Dump Procedure
+			StringBuilder DataDump = new StringBuilder();
+			Diagnostics.FormatDumpData(ref DataDump, "ConsoleProvider", ConsoleProvider.ToString() + Environment.NewLine);
+			Diagnostics.FormatDumpData(ref DataDump, "Console", Console.ToString() + Environment.NewLine);
+			Diagnostics.FormatDumpData(ref DataDump, "ArgBuffer", ArgBuffer);
+			Diagnostics.FormatDumpData(ref DataDump, "ConsoleBuffers", ConsoleBuffers);
+			Diagnostics.FormatDumpData(ref DataDump, "UpdateEvents", UpdateEvents);
+
+			Diagnostics.FormatDumpData(ref DataDump, "Runtime.UpdateFrequency", Runtime.UpdateFrequency);
+			Diagnostics.FormatDumpData(ref DataDump, "updateSource", e.updateSource);
+
+			// Save and Notify
+			Runtime.UpdateFrequency = UpdateFrequency.None;
+			Me.CustomData = DataDump.ToString();
+			Echo($"Data Dumped into Custom Data.{Environment.NewLine}Execution Stopped.");
+			ConsoleBuffers["DataDump"] = $"Data Dumped into CustomData.{Environment.NewLine}Execution Stopped.";
+		}
+	}
+}
--- CSS - SBS-S2M4/Program.cs
+++ CSS - SBS-S2M4/Program.cs
@@ -17,59 +17,149 @@ using VRage.Game;
 using VRage;
 using VRageMath;
 using CSS_Common;
+using System.Reflection;
+using BulletXNA;
 
 namespace IngameScript {
 	partial class Program : MyGridProgram {
+		//#pragma warning disable IDE0044 // Add readonly modifier // The IDE cannot detect that these are modified at runtime by a reference. MS pls Fix ❤
 		IMyTextSurfaceProvider ConsoleProvider;
 		IMyTextSurface Console;
+		IMyCockpit Cockpit;
+		IMyCameraBlock Camera;
+		MyDetectedEntityInfo LastTargetData;
+		Vector3D MeGroundPos;
 		Queue<string> ArgBuffer = new Queue<string>();
 		Dictionary<string, string> ConsoleBuffers = new Dictionary<string, string>();
 		UpdateEventHandlers UpdateEvents = new UpdateEventHandlers();
+		//#pragma warning restore IDE0044 // Add readonly modifier
 
 		void PopHKHistory(object Sender, UpdateEventArgs e) {
 			if (ArgBuffer.Count > 0) ConsoleBuffers["HKHistory"] = e.updateSource.ToString() + ArgBuffer.Dequeue();
 		}
-		void DumpHKHistory(object Sender, UpdateEventArgs e) {
-			Echo("DumpHKHistory");
-			ConsoleBuffers["Terminal"] = ArgBuffer.Aggregate("Terminal", (History, Item) => History + Environment.NewLine + Item);
-			ArgBuffer.Clear();
-		}
-		void AddHKHistory(object Sender, UpdateEventArgs e) {
+
+
+		void AddArgToBuffer(object Sender, UpdateEventArgs e) {
 			ArgBuffer.Enqueue(e.argument);
+			ConsoleBuffers["AddArgToBuffer"] = e.argument;
 		}
 		void Pinger(object Sender, UpdateEventArgs e) {
 			Echo("Ping!: " + e.argument + ", " + e.updateSource.ToString());
 		}
+		void GetTargetData(object Sender, UpdateEventArgs e) {
+			if (!e.argument.Equals("HK:1:1")) return;
+			if (!Camera.CanScan(4000)) return;
+			LastTargetData = Camera.Raycast(4000);
+		}
+		double ProjectileGroundInterceptTime(double RelativeTargetHeight) {
+			Vector3D PlanetPos;
+			Cockpit.TryGetPlanetPosition(out PlanetPos);
+			Vector3D MeVelocity = Cockpit.GetShipVelocities().LinearVelocity;
+			Vector3D MeGravity = Cockpit.GetNaturalGravity();
 
+			double S, U, V, A, T; // Equations of motion in SUVAT notation.
+			S = RelativeTargetHeight;
+			A = MeGravity.Length();
+			U = MeVelocity.Dot(Vector3D.Normalize(MeGravity)) / A; //Extract down vector
+
+			// S = U * T + 0.5 * A T*T    // T = (-U +Math.Sqrt( U*U -4*(0.5*A)*(-S) ))/(2*0.5*A)
+			T = /*Math.Max(*/(-U + Math.Sqrt(U * U - 2 * A * S)) / A/*,(-U - Math.Sqrt(U * U - 2 * A * S)) / A)*/;
+			return T;
+		}
+		double ProjectileLateralTravelDistance(double TimeIntercept) {
+			Vector3D PlanetPos;
+			Cockpit.TryGetPlanetPosition(out PlanetPos);
+			Vector3D MeVelocity = Cockpit.GetShipVelocities().LinearVelocity;
+			Vector3D MeGravity = Cockpit.GetNaturalGravity();
+			Vector3D ToTarget = (LastTargetData.HitPosition ?? MeGroundPos) - MeGroundPos;
+			//ConsoleBuffers["LastTargetData"] = $"LastTargetData: {(LastTargetData.HitPosition ?? MeGroundPos).ToString("F0")}";
+			//ConsoleBuffers["MeGroundPos"] = $"MeGroundPos: {MeGroundPos.ToString("F0")}";
+			//ConsoleBuffers["ToTarget"] = $"ToTarget: {ToTarget.ToString("F0")}";
+
+			double S, U, A, T; // Equations of motion in SUVAT notation.
+			A = MeGravity.Dot(Vector3D.Normalize(ToTarget));
+			U = MeVelocity.Dot(Vector3D.Normalize(ToTarget));
+			T = TimeIntercept;
+			S = (U * T) + (0.5 * A * T * T);
+			return S;
+		}
+		double TimeToVector(Vector3D Pos, Vector3D Target, Vector3D LinearVelocity) {
+			Vector3D ToTarget = Target - Pos;
+			double S, U, V, A, T; // Equations of motion in SUVAT notation.
+			U = LinearVelocity.Dot(Vector3D.Normalize(ToTarget));
+			S = Vector3D.Distance(Pos, Target);
+			T = S / U;
+			return T;
+		}
+		void UpdateReletiveTargetData(object Sender, UpdateEventArgs e) { // Only works in atmosphere
+			Vector3D PlanetPos;
+			Cockpit.TryGetPlanetPosition(out PlanetPos);
+			Vector3D MePos = Me.CubeGrid.GetPosition();
+			Vector3D TargetPos = LastTargetData.HitPosition ?? MePos;
+			Vector3D MeVelocity = Cockpit.GetShipVelocities().LinearVelocity;
+			Vector3D MeGravity = Cockpit.GetNaturalGravity();
+			double Distance = Vector3D.Distance(TargetPos, MePos);
+			double MeHeight = Vector3D.Distance(MePos, PlanetPos);
+			double TargetHeight = Vector3D.Distance(TargetPos, PlanetPos);
+			double RelativeTargetHeight = TargetHeight - MeHeight; // Should be negative when aircraft is above
+
+			Vector3D RelativePlanetPos = PlanetPos - MePos;
+			Vector3D MeRelativeGroundPos;
+			MeRelativeGroundPos = RelativePlanetPos * (1 - (TargetHeight / MeHeight));
+			MeGroundPos = MePos + MeRelativeGroundPos;
+
+			double S, U, V, A, T; // Equations of motion in SUVAT notation.
+			T = ProjectileGroundInterceptTime(RelativeTargetHeight);
+			S = ProjectileLateralTravelDistance(T);
+			double DropDistance = Math.Sqrt((S * S) + (RelativeTargetHeight * RelativeTargetHeight));
+			double GroundDistanceToDrop = Vector3D.Distance(MeGroundPos, TargetPos) - S;
+
+			Vector3D RelativePlanetPosFromTarget = PlanetPos - MePos;
+			Vector3D RelativeDropPosFromTarget;
+
+			double TimeToDrop = TimeToVector(MeGroundPos, TargetPos, Cockpit.GetShipVelocities().LinearVelocity);
+
+			ConsoleBuffers["GroundIntercept"] = $"GroundIntercept: {T:F0}s";
+			ConsoleBuffers["GlideDistance"] = $"GlideDistance: {S:F0}m";
+			ConsoleBuffers["DropFrom"] = $"DropFrom: {DropDistance:F0}m";
+			ConsoleBuffers["GroundDistanceUntillDrop"] = $"GroundDistanceUntillDrop: {GroundDistanceToDrop:F0}m";
+			ConsoleBuffers["TimeToDrop"] = $"TimeToDrop: {TimeToDrop:F0}s";
+		}
 
 		public Program() {
-			Runtime.UpdateFrequency = UpdateFrequency.Update10 | UpdateFrequency.Update100;
+			Runtime.UpdateFrequency = UpdateFrequency.Once;
 
 			TerminalSystem.GetBlockWithName(this, "SBS-Console", ref ConsoleProvider);
+			TerminalSystem.GetBlockWithName(this, "SBS2 Camera Nose", ref Camera);
+			TerminalSystem.GetBlockWithName(this, "SBS2 Fighter Cockpit ", ref Cockpit);
 			Console = ConsoleProvider.GetSurface(0);
 			Console.ContentType = ContentType.TEXT_AND_IMAGE;
 			Console.Alignment = TextAlignment.CENTER;
-			/*
-			UpdateEvents.None += Pinger;
-			UpdateEvents.Terminal += DumpHKHistory;
-			UpdateEvents.Trigger += AddHKHistory;
-			UpdateEvents.Update100 += PopHKHistory;
-			*/
+			ConsoleBuffers.Clear();
 
-			UpdateEvents.Subscribers[Pinger] = UpdateType.None;
-			UpdateEvents.Subscribers[DumpHKHistory] = UpdateType.Terminal;
-			UpdateEvents.Subscribers[AddHKHistory] = UpdateType.Trigger;
-			UpdateEvents.Subscribers[PopHKHistory] = UpdateType.None;
+			Camera.EnableRaycast = true;
+
+			UpdateType UpdatesWithArgs =
+				UpdateType.Terminal |
+				UpdateType.Trigger |
+				UpdateType.Script |
+				UpdateType.IGC;
+			UpdateType UpdatesTick =
+				UpdateType.Update1 |
+				UpdateType.Update10 |
+				UpdateType.Update100;
+			UpdateEvents.Subscribers[AddArgToBuffer] = new EventHandlerMeta(UpdatesWithArgs, "AddArgToBuffer");
+			UpdateEvents.Subscribers[DumpData] = new EventHandlerMeta(UpdatesWithArgs, "DumpData");
+			UpdateEvents.Subscribers[GetTargetData] = new EventHandlerMeta(UpdatesWithArgs, "GetTargetData");
+			UpdateEvents.Subscribers[UpdateReletiveTargetData] = new EventHandlerMeta(UpdatesTick, "UpdateReletiveTargetData");
 		}
 
 		public void Save() {
 		}
 
 		public void Main(string argument, UpdateType updateSource) {
-			ConsoleBuffers["updateSource"] = "updateSource: " + updateSource.ToString();
-
+			Runtime.UpdateFrequency = UpdateFrequency.Update1;
 			UpdateEvents.CallUpdateEventHandlers(argument, updateSource);
-
 			Console.WriteText(
 				ConsoleBuffers.Aggregate("", (TextBuffer, Pair) => TextBuffer + Pair.Value + Environment.NewLine)
 			);
